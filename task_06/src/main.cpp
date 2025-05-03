d2bae7d6edb54a1b714f9079afa5a25ea60c68d1
love <pogodaeva.la@phystech.edu>
2025-05-03 08:48:40 +0000

    clean

--- task_06/src/main.cpp
+++ task_06/src/main.cpp
@@ -2,10 +2,4 @@
 
 #include "search_statistic.hpp"
 
-int main() {
-  std::vector<double> v = {5, 3, 1, 4, 2};
-  std::cout << SearchNStatistics(v, 0) << std::endl;  // 1
-  std::cout << SearchNStatistics(v, 2) << std::endl;  // 3
-  std::cout << SearchNStatistics(v, 4) << std::endl;  // 5
-  return 0;
-}
+int main() { return 0; }
