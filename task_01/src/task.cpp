2157390c0dcb186f005124d85389f53702e6589b
love <pogodaeva.la@phystech.edu>
2025-05-04 19:57:54 +0000

    task_01

---
+++ task_01/src/task.cpp
@@ -0,0 +1,22 @@
+#include "task.hpp"
+
+#include <stdexcept>
+
+std::pair<int, int> search_numbers(int search_num, std::vector<int> data) {
+  if (data.size() <= 1) {
+    throw std::invalid_argument("vector is too small");
+  }
+  size_t bottom{0};
+  size_t top{data.size() - 1};
+  while (bottom != top) {
+    if (data[bottom] + data[top] == search_num) {
+      std::pair<int, int> ans{data[bottom], data[top]};
+      return ans;
+    } else if (data[bottom] + data[top] > search_num) {
+      --top;
+    } else {
+      ++bottom;
+    }
+  }
+  throw std::invalid_argument("no suitable pair");
+}
---
+++ task_01/src/task.hpp
@@ -0,0 +1,4 @@
+#include <utility>
+#include <vector>
+
+std::pair<int, int> search_numbers(int search_num, std::vector<int> data);
