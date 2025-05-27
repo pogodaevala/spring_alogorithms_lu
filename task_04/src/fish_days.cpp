37ea51cb897c00e145c7b1d4fcdfd056c5ef030a
love <pogodaeva.la@phystech.edu>
2025-05-27 21:18:50 +0000

    better fish

--- task_04/src/fish_days.cpp
+++ task_04/src/fish_days.cpp
@@ -2,30 +2,25 @@
 
 std::vector<int> calculate_fish_days(int N, int K,
                                      const std::vector<double>& prices) {
-  if (prices.size() == 0) {
-    return {};
-  }
-  std::vector<double> relevant_prices(K);
+  if (prices.empty()) return {};
+
   std::vector<int> purchases(N, 0);
-  std::vector<int> actual_days(K);
-  relevant_prices[0] = prices[0];
-  actual_days[0] = 0;
-  double min_price = relevant_prices[0];
   int min_price_day = 0;
-  ++purchases[min_price_day];
-  for (int i{1}; i < N; ++i) {
-    relevant_prices[i % K] = prices[i];
-    actual_days[i % K] = i;
-    if (prices[i] <= min_price) {
+  double min_price = prices[0];
+  ++purchases[0];
+
+  for (int i = 1; i < N; ++i) {
+    if (prices[i] <= min_price) {  // если новый минимум
       min_price = prices[i];
       min_price_day = i;
-    } else if (min_price_day <= i - K) {
-      min_price = relevant_prices[0];
-      min_price_day = actual_days[0];
-      for (int j{1}; j < K; ++j) {
-        if (relevant_prices[j] <= min_price) {
-          min_price = relevant_prices[j];
-          min_price_day = actual_days[j];
+    } else if (min_price_day <= i - K) {  // если минимум устарел
+      min_price = prices[i - K + 1];
+      min_price_day = i - K + 1;
+      for (int j = i - K + 2; j <= i;
+           ++j) {  // пересчёт минимума в окне [i-K+1, i]
+        if (prices[j] <= min_price) {
+          min_price = prices[j];
+          min_price_day = j;
         }
       }
     }
